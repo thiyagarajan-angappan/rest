ae31590f2c8e416fdc49c0558889a9e6c54ffa9d
thiyagarajan.angappan <thiyagarajan.angappan@gmail.com>
2019-05-21 16:22:09 -0500

    updating apm attacher

--- RestAPMProject/src/com/rest/apm/APMDemoApplication.java
+++ RestAPMProject/src/com/rest/apm/APMDemoApplication.java
@@ -11,6 +11,15 @@ public class APMDemoApplication extends Application {
 	
 	@Override
 	public Set<Class<?>> getClasses() {
+		
+		/*Map<String, String> apmConfig = new HashMap<String, String>();
+		apmConfig.put("service_name", "apmdemoapp");
+		apmConfig.put("server_urls", "http://127.0.0.1:8200");
+		apmConfig.put("application_packages", "com.rest.apm");
+		System.out.println("---> attaching apm");
+		ElasticApmAttacher.attach(apmConfig);
+		System.out.println("---> attached apm");*/
+		
 		Set<Class<?>> resources = new HashSet<Class<?>>();
 		resources.add(APMDemoRS.class);
 		
